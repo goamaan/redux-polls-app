744c5cb045e7351d2db8137fda9e5e542987ae82
Amaan18 <amaangokak18@gmail.com>
2020-05-29 04:44:14 -0700

    added router, finished app

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,13 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import { handleInitialData } from '../actions/shared';
 import { connect } from 'react-redux';
 import Dashboard from './Dashboard';
 import LoadingBar from 'react-redux-loading';
+import Leaderboard from './Leaderboard';
+import AddPoll from './AddPoll';
+import Poll from './Poll';
+import Nav from './Nav';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 class App extends Component {
   componentDidMount() {
@@ -10,10 +15,22 @@ class App extends Component {
   }
   render() {
     return (
-      <div>
-        <LoadingBar />
-        {this.props.loading === true ? null : <Dashboard />}
-      </div>
+      <Router>
+        <Fragment>
+          <LoadingBar />
+          <div className="container">
+            <Nav />
+            {this.props.loading === true ? null : (
+              <div>
+                <Route path="/" exact component={Dashboard} />
+                <Route path="/leaderboard" component={Leaderboard} />
+                <Route path="/polls/:id" component={Poll} />
+                <Route path="/add" component={AddPoll} />
+              </div>
+            )}
+          </div>
+        </Fragment>
+      </Router>
     );
   }
 }
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,4 +1,6 @@
 import { RECEIVE_USERS } from '../actions/users';
+import { ADD_POLL } from '../actions/polls';
+import { ADD_ANSWER } from '../actions/answers';
 
 export default function polls(state = {}, action) {
   switch (action.type) {
@@ -6,6 +8,25 @@ export default function polls(state = {}, action) {
       return {
         ...state,
         ...action.users,
+      };
+    case ADD_POLL:
+      const poll = action.poll;
+      const { author, id } = poll;
+      return {
+        ...state,
+        [author]: {
+          ...state[author],
+          polls: state[author].polls.concat([id]),
+        },
+      };
+    case ADD_ANSWER:
+      const user = state[action.authedUser];
+      return {
+        ...state,
+        [action.authedUser]: {
+          ...user,
+          answers: user.answers.concat([action.id]),
+        },
       };
     default:
       return state;
